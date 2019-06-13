83d71764e8535a1dfc451b6031222cf7234b9a43
kutycoi123 <kutycoi123@gmail.com>
2019-06-12 23:04:37 -0700

    Initialize auth handlers (#5)

---
+++ routes/auth.route.js
@@ -0,0 +1,36 @@
+var express = require("express");
+var passport = require("passport");
+var WcaStrategy = require("passport-wca");
+var router = express.Router();
+var config = require("../config");
+
+router.use(passport.initialize());
+router.use(passport.session());
+
+passport.serializeUser(function (user, done) {
+    done(null, user);
+});
+
+passport.deserializeUser(function (user, done) {
+    done(null, user);
+});
+
+passport.use(new WcaStrategy(config,
+    function (accessToken, refreshToken, profile, done) {
+        //Need to handle user profile here
+        done(null, profile);
+    }
+));
+
+router.get('/wca',
+    passport.authenticate('wca')
+);
+
+router.get('/wca/callback', 
+    passport.authenticate('wca', {failureRedirect: '/'}),
+    function(req, res){
+        res.send(req.session);
+    }
+)
+
+module.exports = router;
